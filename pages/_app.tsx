11719e2b91dcfc279a738dbb5d8ffc3e5600a95c
Joseph Turk <Joseph.Turk@tibercreek.com>
2022-06-07 15:45:39 -0500

    Add tab nav implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,62 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Box, CssBaseline, Tab, Tabs } from "@mui/material";
+import React from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const links = [
+    {
+        href: "/",
+        label: "Home"
+    },
+    {
+        href: "/about",
+        label: "About"
+    },
+    {
+        href: "/other",
+        label: "Other"
+    }
+];
+
+const LinkTab: React.FunctionComponent<{
+    href: string;
+    label: string;
+}> = props => {
+    return (
+        <Link passHref {...props}>
+            <Tab {...props} />
+        </Link>
+    );
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const router = useRouter();
+    const [value, setValue] = React.useState(
+        links.map(x => x.href).indexOf(router.pathname)
+    );
+
+    const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <>
+            <Box sx={{ width: "100%" }}>
+                <Tabs value={value} onChange={handleChange}>
+                    {links.map(link => (
+                        <LinkTab
+                            href={link.href}
+                            key={link.href}
+                            label={link.label}
+                        />
+                    ))}
+                </Tabs>
+            </Box>
+            <Component {...pageProps} />
+            <CssBaseline />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/other.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Other: NextPage = () => {
+    return (
+        <>
+            <h1>Other</h1>
+        </>
+    );
+};
+
+export default Other;
